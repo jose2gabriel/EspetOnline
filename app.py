0b2c983b5f2f1926898591472288f60702c189a9
jose2gabriel <regencygabriel45@gmail.com>
2025-10-29 01:12:10 -0300

    coisas novas

--- app.py
+++ app.py
@@ -9,22 +9,31 @@ SENHA = "1234"
 USUARIO2 = "biel"
 SENHA2 = "1234"
 
+#armazena os informaçoes de login dos garçons
+garcons = {}
+
 @app.route("/", methods=["GET", "POST"])
 def login():
     erro = ""
     if request.method == "POST":
         usuario = request.form.get("usuario")
         senha = request.form.get("senha")
+        #login de usuario
         if usuario == USUARIO and senha == SENHA:
             return redirect(url_for("home"))
         else:
             erro = "Usuário ou senha incorretos"
-
+        #login de admin
         if usuario == USUARIO2 and senha == SENHA2:
             return redirect(url_for("admin"))
         else:
             erro = "Usuário ou senha incorretos"
-            
+        #login de garçon
+        if usuario in garcons and senha == garcons[usuario]["senha"]:
+            return redirect(url_for("garcom"))
+
+        erro = "Usuário ou senha incorretos"
+
     return render_template("login.html", erro=erro)
 
 @app.route("/home")
@@ -34,9 +43,24 @@ def home():
 
 @app.route("/admin", methods=["GET", "POST"])
 def admin():
-     erro = ""
-     
-    return render_template("admin.html")
+    erro = ""
+    msg = ""
+    if request.method == "POST":
+        # cria um login para o garçon
+        novo_user = request.form.get("novo_usuario")
+        nova_senha = request.form.get("nova_senha")
+
+        if novo_user in garcons:
+            erro = "Esse garçom já existe!"
+        else:
+            garcons[novo_user] = {"senha": nova_senha}
+            msg = f"Garçom {novo_user} criado com sucesso!"
+    return render_template("admin.html", erro=erro, msg=msg, garcons=garcons)
+
+@app.route("/garcom")
+def garcom():
+    return render_template("garcom.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
